61f746d014e872915de489f645ee91fbc6460c61
edmond <edmond.wanyama@student.moringaschool.com>
2023-07-28 11:41:26 +0300

    completed assignment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Signup from "./Signup";
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/message" element={<Message />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Message() {
+  return (
+    <div>
+      <h1>
+       Welcome On Board
+       We Love You, Kindly Make Yourself Comfortable.
+      </h1>
+    </div>
+  );
+}
+
+export default Message;
